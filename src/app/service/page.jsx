da1a8137ca21c7db563deaf7355a51b2dccd0c73
Juvielone Lagos <62714904+juvielone@users.noreply.github.com>
2024-12-17 00:50:19 +0800

    Modify and Added Styles

--- src/app/service/page.jsx
+++ src/app/service/page.jsx
@@ -7,30 +7,27 @@ const Service = () => {
     <>
       <div className="sm:w-11/12 mx-auto pb-10">
         <Navbar />
-        <h1 className="md:text-2xl w-11/12 font-bold mt-10  text-center mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing{" "}
+        <h1 className="text-2xl md:text-4xl w-11/12 font-bold mt-10  text-center mx-auto">
+          <span className="text-[#29AAE3]">Broadtech Solutions</span> Services
         </h1>
 
         {/* Wireband */}
         <div id="wireband" className="mt-20 sm:grid grid-cols-1 md:grid-cols-2">
-          <div>
+          <div
+            className="hover:bg-slate-100 rounded
+          hover:backdrop-blur-lg transition-all duration-300 ease-in-out"
+          >
             <h2 className="md:text-xl w-11/12 font-semibold  pb-5 mx-auto">
               Wideband Services
             </h2>
             <p className="w-11/12 mx-auto pb-5">
-              Established by professionals with more than 25 years of solid
-              experience and in-depth knowledge in the provisioning,
-              commissioning and maintenance of telecommunications services using
-              the traditional copper, fibre optics and wireless technologies.
-              Our company has the reputation for quality workmanship, always
-              ready to listen and exceeds customer’s expectations. We have close
-              alliance with major providers of technical trainings to ensure our
-              staff are abreast with the technologies in demand as well as pool
-              of test equipments to get our job done. We are sub-contractor of
-              major companies partnered in rolling out of NBN's
-              Fibre-to-the-Premises and Hybrid technologies for National
-              Broadband Network Construction Project and long time partner of
-              VisionStream/Telstra in their WideBand Telecommunication project.
+              We specialize in the installation and commissioning of Telstra
+              Wideband Services leveraging traditional copper and modern fibre
+              technologies. Our team ensures seamless integration and optimal
+              performance, providing tailored solutions to meet diverse
+              connectivity needs. With a focus on precision and reliability, we
+              help businesses achieve robust wideband connectivity for their
+              operations.
             </p>
           </div>
           <div>
@@ -55,48 +52,42 @@ const Service = () => {
               alt="brands"
             />
           </div>
-          <div>
+          <div
+            className="hover:bg-slate-100 rounded
+          hover:backdrop-blur-lg transition-all duration-300 ease-in-out"
+          >
             <h2 className="md:text-xl w-11/12 font-semibold  pb-5 mx-auto">
               LAN and IT Infrastructure
             </h2>
             <p className="w-11/12 mx-auto pb-5">
-              Established by professionals with more than 25 years of solid
-              experience and in-depth knowledge in the provisioning,
-              commissioning and maintenance of telecommunications services using
-              the traditional copper, fibre optics and wireless technologies.
-              Our company has the reputation for quality workmanship, always
-              ready to listen and exceeds customer’s expectations. We have close
-              alliance with major providers of technical trainings to ensure our
-              staff are abreast with the technologies in demand as well as pool
-              of test equipments to get our job done. We are sub-contractor of
-              major companies partnered in rolling out of NBN's
-              Fibre-to-the-Premises and Hybrid technologies for National
-              Broadband Network Construction Project and long time partner of
-              VisionStream/Telstra in their WideBand Telecommunication project.
+              Our LAN and IT infrastructure services cover every aspect of
+              design, installation, commissioning, and maintenance. Whether it's
+              setting up new networks, optimizing existing systems, or ensuring
+              ongoing operational efficiency, we deliver cutting-edge solutions.
+              From structured cabling to advanced network management, we cater
+              to businesses of all sizes, ensuring secure and scalable IT
+              environments to support their growth.
             </p>
           </div>
         </div>
 
         {/* Copper */}
         <div id="copper" className="mt-20 sm:grid grid-cols-1 md:grid-cols-2">
-          <div>
+          <div
+            className="hover:bg-slate-100 rounded
+          hover:backdrop-blur-lg transition-all duration-300 ease-in-out"
+          >
             <h2 className="md:text-xl w-11/12 font-semibold  pb-5 mx-auto">
               Copper and Fibre Install
             </h2>
             <p className="w-11/12 mx-auto pb-5">
-              Established by professionals with more than 25 years of solid
-              experience and in-depth knowledge in the provisioning,
-              commissioning and maintenance of telecommunications services using
-              the traditional copper, fibre optics and wireless technologies.
-              Our company has the reputation for quality workmanship, always
-              ready to listen and exceeds customer’s expectations. We have close
-              alliance with major providers of technical trainings to ensure our
-              staff are abreast with the technologies in demand as well as pool
-              of test equipments to get our job done. We are sub-contractor of
-              major companies partnered in rolling out of NBN's
-              Fibre-to-the-Premises and Hybrid technologies for National
-              Broadband Network Construction Project and long time partner of
-              VisionStream/Telstra in their WideBand Telecommunication project.
+              Broadtech Business Solutions brings extensive expertise in
+              end-to-end copper and fibre network solutions. From initial
+              installation to rigorous testing and ongoing maintenance, our
+              services ensure high-performance connectivity. Whether you're
+              upgrading infrastructure or building a new network, we deliver
+              exceptional results, meeting industry standards for quality and
+              reliability.
             </p>
           </div>
           <div>
@@ -121,24 +112,22 @@ const Service = () => {
               alt="brands"
             />
           </div>
-          <div>
+          <div
+            className="hover:bg-slate-100 rounded
+          hover:backdrop-blur-lg transition-all duration-300 ease-in-out"
+          >
             <h2 className="md:text-xl w-11/12 font-semibold  pb-5 mx-auto">
               Carrier Infrastructure Projects
             </h2>
             <p className="w-11/12 mx-auto pb-5">
-              Established by professionals with more than 25 years of solid
-              experience and in-depth knowledge in the provisioning,
-              commissioning and maintenance of telecommunications services using
-              the traditional copper, fibre optics and wireless technologies.
-              Our company has the reputation for quality workmanship, always
-              ready to listen and exceeds customer’s expectations. We have close
-              alliance with major providers of technical trainings to ensure our
-              staff are abreast with the technologies in demand as well as pool
-              of test equipments to get our job done. We are sub-contractor of
-              major companies partnered in rolling out of NBN's
-              Fibre-to-the-Premises and Hybrid technologies for National
-              Broadband Network Construction Project and long time partner of
-              VisionStream/Telstra in their WideBand Telecommunication project.
+              We handle comprehensive carrier infrastructure projects, including
+              node installations, system upgrades, commissioning and
+              decommissioning, intra-cabling, monitoring, and disaster recovery.
+              Our expertise spans a broad range of technologies such as SDH/PDH,
+              Frame Relay, TDM, ISDN, Optumux, OMS, and various Cisco routers
+              and switches. With a focus on scalability and resilience, we
+              deliver solutions that empower carriers to maintain and enhance
+              their network operations.
             </p>
           </div>
         </div>
